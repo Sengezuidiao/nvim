5a2a610bd2f35559d0f1ce16b82dfdbc6046a41d
GGBOND <1454538956@qq.com>
2025-01-20 23:55:59 +0800

    add .

--- lua/plugins/coc-nvim.lua
+++ lua/plugins/coc-nvim.lua
@@ -1,7 +1,8 @@
-return{
+return {
   "neoclide/coc.nvim",
-  branch="release",
-  config = function ()
-    vim.g.coc_global_extensions = {"coc-json", "coc-clangd", "coc-markdownlint", "coc-cmake", "coc-translator", "coc-lua", "coc-stylua", }
+  branch = "release",
+  config = function()
+    vim.g.coc_global_extensions = { "coc-json", "coc-clangd", "coc-markdownlint", "coc-cmake", "coc-translator",
+      "coc-lua", "coc-stylua", }
   end,
 }
