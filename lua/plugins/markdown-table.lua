66e405d7fb377ad290e30b1c009e6fafe794ee5f
GGBOND <1454538956@qq.com>
2025-01-20 23:57:57 +0800

    add .

--- lua/plugins/markdown-table.lua
+++ lua/plugins/markdown-table.lua
@@ -1,14 +1,13 @@
-return{
+return {
   "dhruvasagar/vim-table-mode",
   config = function()
     local group = vim.api.nvim_create_augroup("markdown_config", { clear = true })
     -- 在markdown文件类型中设置按键映射
-    vim.api.nvim_create_autocmd("FileType",{
+    vim.api.nvim_create_autocmd("FileType", {
       group = group,
       pattern = "markdown",
       callback = function()
         vim.api.nvim_buf_set_keymap(0, "n", "<leader>tr", ":TableModeRealign<CR>", { noremap = true, silent = true })
-
       end
     })
 
