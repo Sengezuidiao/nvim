421c3b2423ece6c64b866fe3d37e622934403dd8
GGBOND <1454538956@qq.com>
2025-01-22 19:12:37 +0800

    1

--- lua/plugins/markdown-image.lua
+++ lua/plugins/markdown-image.lua
@@ -4,8 +4,9 @@ return {
 		vim.cmd([[
 autocmd FileType markdown nmap <buffer><silent> <leader>p :call mdip#MarkdownClipboardImage()<CR>
 " there are some defaults for image directory and image name, you can change them
-let g:mdip_imgdir = './img'
+let g:mdip_imgdir = './assets'
 let g:mdip_imgname = 'image'
+let g:mdip_imgdir_
   ]])
 	end,
 }
