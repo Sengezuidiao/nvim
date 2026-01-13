4ca344a11727bdf2c5a0e6c353456cb11e9cfe16
ggbond <1454538956@qq.com>
2026-01-13 00:44:56 +0000

    compile and run
    modify c run

--- lua/config/compilerun.lua
+++ lua/config/compilerun.lua
@@ -11,7 +11,7 @@ function compile_and_run()
 		vim.o.splitbelow = true
 		vim.cmd("split | resize -5")
 		-- 打开终端并编译运行
-		vim.cmd("term gcc " .. filename .. " -o " .. output .. " && time ./" .. output)
+		vim.cmd("term gcc " .. filename .. " -o " .. output .. " && ./" .. output)
 	elseif filetype == "cpp" then
 		-- 编译
 		vim.fn.system("g++ -std=c++11 " .. filename .. " -Wall -o " .. output)
